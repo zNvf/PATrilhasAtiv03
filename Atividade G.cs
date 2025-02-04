513aea3c71e74bd49f48eb12adbc58a4df4ab131
zNvf <tradelost2@gmail.com>
2025-02-04 11:03:17 -0300

    Create Atividade G.cs

---
+++ Atividade G.cs
@@ -0,0 +1,24 @@
+using System;
+namespace PrimeiraAtividade
+{
+class Program
+{
+public static void Main()
+{	
+Console.Write("Digite o primeiro número: ");
+float numero = Convert.ToSingle(Console.ReadLine());			
+Console.Write("Digite o segundo número: ");
+float numero2 = Convert.ToSingle(Console.ReadLine());				
+if (numero < numero2)
+{ 
+Console.WriteLine("\n\rO primeiro número é menor que o segundo");
+}
+else if (numero == numero2)
+{
+Console.WriteLine("\n\rOs números são iguais");
+}
+Console.WriteLine("\n\rAperte alguma tecla para fechar...");
+Console.ReadKey(true);
+}
+}
+}
